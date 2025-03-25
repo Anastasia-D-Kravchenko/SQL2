ddd4802bc9e03fc955a6c984eab0f82a7b73b08f
Anastasia Kravchenko <118124554+Anastasia-D-Kravchenko@users.noreply.github.com>
2025-03-25 01:22:18 +0100

    renewed

--- project/modify.sql
+++ project/modify.sql
@@ -1,63 +1,127 @@
---a. SELECT with JOIN and WHERE (3 Queries):
--- 1. Find transactions that failed validation
-SELECT t.id, t.Amount, mvl.Timestamp
-FROM Transaction t
-         JOIN MESSAGE_VALIDATION_LOG mvl ON t.id = mvl.Transaction_id
-WHERE mvl.Error_Message_Code IS NOT NULL;
--- 2. Find user logins from a specific IP
-SELECT ul.User_id, ul.Timestamp, a.SourceIP
-FROM User_Log ul
-         JOIN Activity a ON ul.User_id = a.User_id
-WHERE a.SourceIP = '192.168.1.100' AND ul.EventType = 'Login';
--- 3. Retrieve transactions with their currency details
-SELECT t.id, t.Amount, c.Currency
-FROM Transaction t
-         JOIN Currency c ON t.Currency_id = c.id
-WHERE t.Amount > 1000;
+-- 1. SELECT statement that joins at least two tables and contains WHERE clause
+-- Find all transactions with an amount greater than 100000 USD.
+SELECT
+    t.id,
+    t.Timestamp,
+    t.Amount,
+    c.CurrencyCode
+FROM
+    Transaction t
+        JOIN
+    Currency c ON t.Currency_CurrencyCode = c.CurrencyCode
+WHERE
+    t.Amount > 100000 AND c.CurrencyCode = 'USD';
 
+-- 2. SELECT statement that joins at least two tables and contains WHERE clause
+-- Find the names of users who have the 'Administrator' role.
+SELECT
+    u.Name,
+    u.Surname
+FROM
+    "User" u
+        JOIN
+    User_Role_Access ura ON u.id = ura.User_id
+        JOIN
+    Role r ON ura.Role_id = r.id
+WHERE
+    r.Role = 'Administrator';
 
---b. SELECT with JOIN, GROUP BY, and HAVING (3 Queries):
--- 1. Count validation errors per message type
-SELECT mt.MessageType, COUNT(*) AS ErrorCount
-FROM MESSAGE_VALIDATION_LOG mvl
-         JOIN Message_Validation_Rule mvr ON mvl.Message_Validation_Rule_id = mvr.id
-         JOIN All_MessageType mt ON mvr.Message_Types_id = mt.id
-GROUP BY mt.MessageType
-HAVING COUNT(*) > 2;
--- 2. Find components with more than 1 error
-SELECT c.Name, COUNT(*) AS ErrorCount
-FROM ERROR_MESSAGE em
-         JOIN Component c ON em.Component_id = c.id
-GROUP BY c.Name
-HAVING COUNT(*) > 1;
--- 3. Find average transaction amount per currency
-SELECT c.Currency, AVG(t.Amount) AS AverageAmount
-FROM Transaction t
-         JOIN Currency c ON t.Currency_id = c.id
-GROUP BY c.Currency
-HAVING AVG(t.Amount) > 100000;
+-- 3. SELECT statement that joins at least two tables and contains WHERE clause
+-- List all alerts that are related to 'Security Breach' and are not yet resolved.
+SELECT
+    a.id,
+    a.Timestamp,
+    em.Message
+FROM
+    Alert a
+        JOIN
+    Error_Message em ON a.Error_Message_Code = em.Code
+WHERE
+    a.Type = 'Security Breach' AND a.Resolved = 0;
 
+-- 4. SELECT statement that joins at least two tables and contains GROUP BY and HAVING clauses
+-- Find the number of transactions for each currency where the count is greater than 1.
+SELECT
+    c.CurrencyCode,
+    COUNT(t.id) AS NumberOfTransactions
+FROM
+    Transaction t
+        JOIN
+    Currency c ON t.Currency_CurrencyCode = c.CurrencyCode
+GROUP BY
+    c.CurrencyCode
+HAVING
+    COUNT(t.id) > 1;
 
---c. SELECT with Subquery (2 Queries):
--- 1. Find transactions with amounts greater than the average
-SELECT id, Amount
-FROM Transaction
-WHERE Amount > (SELECT AVG(Amount) FROM Transaction);
--- 2. Find users who have logged in more than once
-SELECT User_id
-FROM User_Log
-WHERE EventType = 'Login'
-GROUP BY User_id
-HAVING COUNT(*) > 1;
+-- 5. SELECT statement that joins at least two tables and contains GROUP BY and HAVING clauses
+-- Find the average transaction amount for each message type, only for averages greater than 90000.
+SELECT
+    mt.MessageType,
+    AVG(t.Amount) AS AverageTransactionAmount
+FROM
+    Transaction t
+        JOIN
+    Message_Type mt ON t.Message_Types_id = mt.id
+GROUP BY
+    mt.MessageType
+HAVING
+    AVG(t.Amount) > 90000;
 
+-- 6. SELECT statement that joins at least two tables and contains GROUP BY and HAVING clauses
+-- Find the number of message validation logs for each error message code, only showing codes with more than one log.
+SELECT
+    em.Message,
+    COUNT(mvl.id) AS NumberOfValidationLogs
+FROM
+    Message_Validation_Log mvl
+        JOIN
+    Error_Message em ON mvl.Error_Message_Code = em.Code
+GROUP BY
+    em.Message
+HAVING
+    COUNT(mvl.id) > 1;
 
---d. SELECT with Correlated Subquery (2 Queries):
--- 1. Find transactions with amounts greater than the average for their currency
-SELECT t.id, t.Amount, c.Currency
-FROM Transaction t
-         JOIN Currency c ON t.Currency_id = c.id
-WHERE t.Amount > (SELECT AVG(Amount) FROM Transaction t2 WHERE t2.Currency_id = t.Currency_id);
--- 2. Find validation logs with errors related to components that also have transaction errors.
-SELECT mvl.*
-FROM MESSAGE_VALIDATION_LOG mvl
-WHERE EXISTS (SELECT 1 FROM ERROR_MESSAGE em WHERE em.Code = mvl.Error_Message_Code AND EXISTS (SELECT 1 FROM TRANSACTION_LOG tl WHERE tl.Error_Message_Code = em.Code));
+-- 7. SELECT statement with subquery
+-- Find all IBANs that are associated with the 'Barclays Bank'.
+SELECT
+    i.IBAN
+FROM
+    IBAN i
+WHERE
+    i.id IN (SELECT IBAN_id FROM IBAN_BIC WHERE BIC_Directory_BIC = (SELECT BIC FROM BIC_Directory WHERE InstitutionName = 'Barclays Bank'));
+
+-- 8. SELECT statement with subquery
+-- Find all users who have access to the 'Account Access' resource.
+SELECT
+    u.Name,
+    u.Surname
+FROM
+    "User" u
+WHERE
+    u.id IN (SELECT User_id FROM User_Role_Access WHERE Access_Control_List_id = (SELECT id FROM Access_Control_List WHERE "Resource" = 'Account Access'));
+
+-- 9. SELECT statement with correlated subquery
+-- Find all transactions that have an amount greater than the average amount for their currency.
+SELECT
+    t.id,
+    t.Amount,
+    c.CurrencyCode
+FROM
+    Transaction t
+        JOIN
+    Currency c ON t.Currency_CurrencyCode = c.CurrencyCode
+WHERE
+    t.Amount > (SELECT AVG(Amount) FROM Transaction WHERE Currency_CurrencyCode = t.Currency_CurrencyCode);
+
+-- 10. SELECT statement with correlated subquery
+-- Find all message validation logs that occurred after the timestamp of their corresponding alert.
+SELECT
+    mvl.id,
+    mvl.Timestamp AS ValidationLogTimestamp,
+    a.Timestamp AS AlertTimestamp
+FROM
+    Message_Validation_Log mvl
+        JOIN
+    Alert a ON mvl.Error_Message_Code = a.Error_Message_Code
+WHERE
+    mvl.Timestamp > a.Timestamp;
