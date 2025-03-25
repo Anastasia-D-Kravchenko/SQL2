9af601e18e2ef1d1a3d8ba28c67a7bcf8762f545
Anastasia Kravchenko <118124554+Anastasia-D-Kravchenko@users.noreply.github.com>
2025-03-25 01:15:38 +0100

    renew

--- project/Security_Logging_and_Monitoring_for_SWIFT_Services_create.sql
+++ project/Security_Logging_and_Monitoring_for_SWIFT_Services_create.sql
@@ -4,22 +4,18 @@ CREATE TABLE Access_Control_List (
     Permission integer  NOT NULL,
     CONSTRAINT Access_Control_List_pk PRIMARY KEY (id)
 ) ;
-CREATE TABLE Activity (
-    id integer  NOT NULL,
-    Timestamp timestamp  NOT NULL,
-    Type varchar2(255)  NOT NULL,
-    ResourceAccessed varchar2(255)  NOT NULL,
-    SourceIP varchar2(46)  NOT NULL,
-    CONSTRAINT Activity_pk PRIMARY KEY (id)
+CREATE TABLE AccountType (
+    id int  NOT NULL,
+    AccountType varchar2(50)  NOT NULL,
+    CONSTRAINT AccountType_pk PRIMARY KEY (id)
 ) ;
-CREATE TABLE All_MessageType (
+CREATE TABLE Alert (
     id integer  NOT NULL,
-    MessageType varchar2(20)  NOT NULL,
-    Category_id integer  NOT NULL,
-    Description varchar2(255)  NOT NULL,
-    StandardVersion varchar2(20)  NOT NULL,
-    DocumentationLink varchar2(255)  NOT NULL,
-    CONSTRAINT All_MessageType_pk PRIMARY KEY (id)
+    Type varchar2(255)  NOT NULL,
+    Timestamp timestamp  NOT NULL,
+    Error_Message_Code integer  NOT NULL,
+    Resolved integer  NOT NULL,
+    CONSTRAINT Alert_pk PRIMARY KEY (id)
 ) ;
 CREATE TABLE BIC_Directory (
     BIC varchar2(11)  NOT NULL,
@@ -31,11 +27,6 @@ CREATE TABLE Category (
     id integer  NOT NULL,
     Category varchar2(18)  NOT NULL,
     CONSTRAINT Category_pk PRIMARY KEY (id)
-) ;
-CREATE TABLE ColorCode (
-    id integer  NOT NULL,
-    ColorCode varchar2(10)  NOT NULL,
-    CONSTRAINT ColorCode_pk PRIMARY KEY (id)
 ) ;
 CREATE TABLE Component (
     id integer  NOT NULL,
@@ -49,82 +40,64 @@ CREATE TABLE Component (
     Configuration xmltype  NOT NULL,
     CONSTRAINT Component_pk PRIMARY KEY (id)
 ) ;
-CREATE TABLE Correlation_Repository (
-    id integer  NOT NULL,
-    Transaction_id integer  NOT NULL,
-    Timestamp timestamp  NOT NULL,
-    MessageDetails varchar2(255)  NOT NULL,
-    DomainName_id integer  NOT NULL,
-    Direction_id integer  NOT NULL,
-    ColorCode_id integer  NOT NULL,
-    Status_id integer  NOT NULL,
-    CONSTRAINT Correlation_Repository_pk PRIMARY KEY (id)
-) ;
 CREATE TABLE Currency (
-    id integer  NOT NULL,
-    Currency varchar2(3)  NOT NULL,
-    CONSTRAINT Currency_pk PRIMARY KEY (id)
-) ;
-CREATE TABLE Direction (
-    id integer  NOT NULL,
-    Direction varchar2(8)  NOT NULL,
-    CONSTRAINT Direction_pk PRIMARY KEY (id)
-) ;
-CREATE TABLE DomainName (
-    id integer  NOT NULL,
-    DomainName varchar2(255)  NOT NULL,
-    CONSTRAINT DomainName_pk PRIMARY KEY (id)
+    CurrencyCode varchar2(3)  NOT NULL,
+    ExchangeRateToUSD number(18,8)  NOT NULL,
+    ExchangeRateLastUpdated timestamp  NOT NULL,
+    IsActive integer  NOT NULL,
+    CONSTRAINT Currency_pk PRIMARY KEY (CurrencyCode)
 ) ;
 CREATE TABLE Error_Message (
     Code integer  NOT NULL,
     Message varchar2(255)  NOT NULL,
     Severity_id integer  NOT NULL,
-    Description varchar2(255)  NOT NULL,
-    Source_Component_id integer  NOT NULL,
     Component_id integer  NOT NULL,
+    EventType_id integer  NOT NULL,
+    Description varchar2(255)  NOT NULL,
     CONSTRAINT Error_Message_pk PRIMARY KEY (Code)
 ) ;
-CREATE TABLE EventType_Massage (
+CREATE TABLE EventType (
     id integer  NOT NULL,
-    EventType varchar2(255)  NOT NULL,
-    CONSTRAINT EventType_Massage_pk PRIMARY KEY (id)
+    Type varchar2(255)  NOT NULL,
+    ResourceAccessed varchar2(255)  NOT NULL,
+    SourceIP varchar2(46)  NOT NULL,
+    CONSTRAINT EventType_pk PRIMARY KEY (id)
 ) ;
-CREATE TABLE EventType_Transaction (
-    id integer  NOT NULL,
-    EventType varchar2(255)  NOT NULL,
-    CONSTRAINT EventType_Transaction_pk PRIMARY KEY (id)
+CREATE TABLE IBAN (
+    id int  NOT NULL,
+    IBAN varchar2(34)  NOT NULL,
+    HolderName varchar2(255)  NOT NULL,
+    AccountNumber varchar2(50)  NOT NULL,
+    Currency_CurrencyCode varchar2(3)  NOT NULL,
+    AccountType_id int  NOT NULL,
+    Status_id integer  NOT NULL,
+    CreationDate date  NOT NULL,
+    CONSTRAINT IBAN_pk PRIMARY KEY (id)
 ) ;
-CREATE TABLE EventType_User (
-    id integer  NOT NULL,
-    EventType varchar2(255)  NOT NULL,
-    CONSTRAINT EventType_User_pk PRIMARY KEY (id)
+CREATE TABLE IBAN_BIC (
+    IBAN_id int  NOT NULL,
+    BIC_Directory_BIC varchar2(11)  NOT NULL,
+    CONSTRAINT IBAN_BIC_pk PRIMARY KEY (IBAN_id,BIC_Directory_BIC)
 ) ;
 CREATE TABLE Message_Type (
     id integer  NOT NULL,
-    MessageType_id integer  NOT NULL,
-    IsMX integer  NOT NULL,
+    MessageType varchar2(10)  NOT NULL,
+    Category_id integer  NOT NULL,
+    Description varchar2(255)  NOT NULL,
+    StandardVersion varchar2(20)  NOT NULL,
+    DocumentationLink varchar2(255)  NOT NULL,
     CONSTRAINT Message_Type_pk PRIMARY KEY (id)
-) ;
-CREATE TABLE Message_Validation_Alert (
-    id integer  NOT NULL,
-    Type varchar2(255)  NOT NULL,
-    Timestamp timestamp  NOT NULL,
-    Resolved integer  NOT NULL,
-    Message_Validation_Log_id integer  NOT NULL,
-    CONSTRAINT Message_Validation_Alert_pk PRIMARY KEY (id)
 ) ;
 CREATE TABLE Message_Validation_Log (
     id integer  NOT NULL,
     Timestamp timestamp  NOT NULL,
     Error_Message_Code integer  NOT NULL,
-    Message_Validation_Rule_id integer  NOT NULL,
-    EventType_Massage_id integer  NOT NULL,
     CONSTRAINT Message_Validation_Log_pk PRIMARY KEY (id)
 ) ;
 CREATE TABLE Message_Validation_Rule (
     id integer  NOT NULL,
     Message_Types_id integer  NOT NULL,
-    Role_id integer  NOT NULL,
+    Rule_id integer  NOT NULL,
     CONSTRAINT Message_Validation_Rule_pk PRIMARY KEY (id)
 ) ;
 CREATE TABLE ReceiverBIC (
@@ -133,11 +106,21 @@ CREATE TABLE ReceiverBIC (
     CONSTRAINT ReceiverBIC_pk PRIMARY KEY (BIC_Directory_BIC,SWIFT_Transactions_id)
 ) ;
 CREATE TABLE Role (
-    id integer  NOT NULL,
-    RuleType varchar2(50)  NOT NULL,
-    RuleDescription varchar2(255)  NOT NULL,
-    RuleExpression varchar2(255)  NOT NULL,
+    id int  NOT NULL,
+    Role varchar2(255)  NOT NULL,
     CONSTRAINT Role_pk PRIMARY KEY (id)
+) ;
+CREATE TABLE Rule (
+    id integer  NOT NULL,
+    Type varchar2(50)  NOT NULL,
+    Expression varchar2(255)  NOT NULL,
+    Description varchar2(255)  NOT NULL,
+    CONSTRAINT Rule_pk PRIMARY KEY (id)
+) ;
+CREATE TABLE RuleLog (
+    Message_Validation_Rule_id integer  NOT NULL,
+    Message_Validation_Log_id integer  NOT NULL,
+    CONSTRAINT RuleLog_pk PRIMARY KEY (Message_Validation_Rule_id,Message_Validation_Log_id)
 ) ;
 CREATE TABLE SenderBIC (
     BIC_Directory_BIC varchar2(11)  NOT NULL,
@@ -149,208 +132,155 @@ CREATE TABLE Severity (
     Severity integer  NOT NULL,
     CONSTRAINT Severity_pk PRIMARY KEY (id)
 ) ;
-CREATE TABLE Source_Component (
-    id integer  NOT NULL,
-    Source_Component varchar2(255)  NOT NULL,
-    CONSTRAINT Source_Component_pk PRIMARY KEY (id)
-) ;
 CREATE TABLE Status (
     id integer  NOT NULL,
     Status varchar2(20)  NOT NULL,
+    Category varchar2(50)  NOT NULL,
     CONSTRAINT Status_pk PRIMARY KEY (id)
-) ;
-CREATE TABLE System_Alert (
-    id integer  NOT NULL,
-    SWIFT_System_Logs_id integer  NOT NULL,
-    Type varchar2(255)  NOT NULL,
-    Timestamp timestamp  NOT NULL,
-    Resolved integer  NOT NULL,
-    CONSTRAINT System_Alert_pk PRIMARY KEY (id)
 ) ;
 CREATE TABLE Transaction (
     id integer  NOT NULL,
     Timestamp timestamp  NOT NULL,
+    Currency_CurrencyCode varchar2(3)  NOT NULL,
     Amount number(20,2)  NOT NULL,
+    Status_id integer  NOT NULL,
     Message_Types_id integer  NOT NULL,
-    Currency_id integer  NOT NULL,
-    Transaction_Status_id integer  NOT NULL,
     Details varchar2(255)  NOT NULL,
     CONSTRAINT Transaction_pk PRIMARY KEY (id)
 ) ;
-CREATE TABLE Transaction_Alert (
-    id integer  NOT NULL,
-    Type varchar2(255)  NOT NULL,
-    Timestamp timestamp  NOT NULL,
-    Resolved integer  NOT NULL,
-    System_Log_id integer  NOT NULL,
-    CONSTRAINT Transaction_Alert_pk PRIMARY KEY (id)
+CREATE TABLE TransactionLog (
+    Transaction_id integer  NOT NULL,
+    Transaction_Log_id integer  NOT NULL,
+    CONSTRAINT TransactionLog_pk PRIMARY KEY (Transaction_id,Transaction_Log_id)
 ) ;
 CREATE TABLE Transaction_Log (
     id integer  NOT NULL,
     Timestamp timestamp  NOT NULL,
     Error_Message_Code integer  NOT NULL,
-    Transaction_id integer  NOT NULL,
-    EventType_Transaction_id integer  NOT NULL,
     CONSTRAINT Transaction_Log_pk PRIMARY KEY (id)
-) ;
-CREATE TABLE Transaction_Status (
-    id integer  NOT NULL,
-    TransactionStatus varchar2(20)  NOT NULL,
-    CONSTRAINT Transaction_Status_pk PRIMARY KEY (id)
 ) ;
 CREATE TABLE "User" (
     id int  NOT NULL,
-    Username varchar2(255)  NULL,
-    Role varchar2(255)  NULL,
+    PESEL char(11)  NOT NULL,
+    Username varchar2(255)  NOT NULL,
+    Name varchar2(20)  NOT NULL,
+    Surname varchar2(20)  NOT NULL,
     CONSTRAINT Users_pk PRIMARY KEY (id)
 ) ;
-CREATE TABLE User_Access (
+CREATE TABLE UserLog (
     User_id int  NOT NULL,
-    Access_Control_List_id integer  NOT NULL,
-    CONSTRAINT User_Access_pk PRIMARY KEY (User_id,Access_Control_List_id)
-) ;
-CREATE TABLE User_Activity (
-    User_id int  NOT NULL,
-    Activity_id integer  NOT NULL,
-    CONSTRAINT User_Activity_pk PRIMARY KEY (User_id,Activity_id)
-) ;
-CREATE TABLE User_BIC (
-    BIC_Directory_BIC varchar2(11)  NOT NULL,
-    Users_id int  NOT NULL,
-    CONSTRAINT User_BIC_pk PRIMARY KEY (BIC_Directory_BIC,Users_id)
+    User_Log_id integer  NOT NULL,
+    CONSTRAINT UserLog_pk PRIMARY KEY (User_id,User_Log_id)
 ) ;
 CREATE TABLE User_Log (
     id integer  NOT NULL,
     Timestamp timestamp  NOT NULL,
-    EventType varchar2(255)  NOT NULL,
     Error_Message_Code integer  NOT NULL,
-    User_id int  NOT NULL,
-    EventType_User_id integer  NOT NULL,
     CONSTRAINT User_Log_pk PRIMARY KEY (id)
 ) ;
-CREATE TABLE XSD_Schemas (
-    id integer  NOT NULL,
-    SchemaName varchar2(255)  NOT NULL,
-    TargetNamespace varchar2(255)  NOT NULL,
-    FilePath varchar2(255)  NOT NULL,
-    Description varchar2(255)  NOT NULL,
-    Version varchar2(20)  NOT NULL,
-    Message_Type_id integer  NOT NULL,
-    CONSTRAINT XSD_Schemas_pk PRIMARY KEY (id)
+CREATE TABLE User_Role_Access (
+    Role_id int  NOT NULL,
+    User_id int  NOT NULL,
+    Access_Control_List_id integer  NOT NULL,
+    IBAN_id int  NOT NULL,
+    CONSTRAINT User_Role_Access_pk PRIMARY KEY (Role_id,User_id,Access_Control_List_id,IBAN_id)
 ) ;
 
 
-ALTER TABLE Transaction_Alert ADD CONSTRAINT Alert_System_Log
-    FOREIGN KEY (System_Log_id)
-    REFERENCES Transaction_Log (id);
-ALTER TABLE All_MessageType ADD CONSTRAINT All_MessageType_Category
-    FOREIGN KEY (Category_id)
-    REFERENCES Category (id);
-ALTER TABLE Correlation_Repository ADD CONSTRAINT Correlation_ColorCode
-    FOREIGN KEY (ColorCode_id)
-    REFERENCES ColorCode (id);
-ALTER TABLE Correlation_Repository ADD CONSTRAINT Correlation_Direction
-    FOREIGN KEY (Direction_id)
-    REFERENCES Direction (id);
-ALTER TABLE Correlation_Repository ADD CONSTRAINT Correlation_DomainName
-    FOREIGN KEY (DomainName_id)
-    REFERENCES DomainName (id);
-ALTER TABLE Correlation_Repository ADD CONSTRAINT Correlation_Repository_Status
-    FOREIGN KEY (Status_id)
-    REFERENCES Status (id);
-ALTER TABLE Correlation_Repository ADD CONSTRAINT Correlation_Transaction
-    FOREIGN KEY (Transaction_id)
-    REFERENCES Transaction (id);
+
+ALTER TABLE Alert ADD CONSTRAINT Alert_Error_Message
+    FOREIGN KEY (Error_Message_Code)
+    REFERENCES Error_Message (Code);
 ALTER TABLE Error_Message ADD CONSTRAINT Error_Message_Component
     FOREIGN KEY (Component_id)
     REFERENCES Component (id);
+ALTER TABLE Error_Message ADD CONSTRAINT Error_Message_EventType
+    FOREIGN KEY (EventType_id)
+    REFERENCES EventType (id);
 ALTER TABLE Error_Message ADD CONSTRAINT Error_Message_Severity
     FOREIGN KEY (Severity_id)
     REFERENCES Severity (id);
-ALTER TABLE Error_Message ADD CONSTRAINT Error_Message_Source_Component
-    FOREIGN KEY (Source_Component_id)
-    REFERENCES Source_Component (id);
+ALTER TABLE IBAN ADD CONSTRAINT IBAN_AccountType
+    FOREIGN KEY (AccountType_id)
+    REFERENCES AccountType (id);
+ALTER TABLE IBAN_BIC ADD CONSTRAINT IBAN_BIC_BIC_Directory
+    FOREIGN KEY (BIC_Directory_BIC)
+    REFERENCES BIC_Directory (BIC);
+ALTER TABLE IBAN ADD CONSTRAINT IBAN_Currency
+    FOREIGN KEY (Currency_CurrencyCode)
+    REFERENCES Currency (CurrencyCode);
+ALTER TABLE IBAN ADD CONSTRAINT IBAN_Status
+    FOREIGN KEY (Status_id)
+    REFERENCES Status (id);
 ALTER TABLE Message_Validation_Log ADD CONSTRAINT Log_Error_Message
     FOREIGN KEY (Error_Message_Code)
     REFERENCES Error_Message (Code);
-ALTER TABLE Message_Validation_Log ADD CONSTRAINT Log_EventType_Massage
-    FOREIGN KEY (EventType_Massage_id)
-    REFERENCES EventType_Massage (id);
-ALTER TABLE Transaction_Log ADD CONSTRAINT Log_EventType_Transaction
-    FOREIGN KEY (EventType_Transaction_id)
-    REFERENCES EventType_Transaction (id);
-ALTER TABLE Message_Validation_Log ADD CONSTRAINT Log_Message_Validation_Rule
-    FOREIGN KEY (Message_Validation_Rule_id)
-    REFERENCES Message_Validation_Rule (id);
-ALTER TABLE Message_Type ADD CONSTRAINT Message_Type_MessageType
-    FOREIGN KEY (MessageType_id)
-    REFERENCES All_MessageType (id);
-ALTER TABLE Message_Validation_Alert ADD CONSTRAINT Message_Validation_Log_Alert
-    FOREIGN KEY (Message_Validation_Log_id)
-    REFERENCES Message_Validation_Log (id);
-ALTER TABLE Message_Validation_Rule ADD CONSTRAINT Message_Validation_Rules_Role
-    FOREIGN KEY (Role_id)
-    REFERENCES Role (id);
+ALTER TABLE Message_Type ADD CONSTRAINT Message_Type_Category
+    FOREIGN KEY (Category_id)
+    REFERENCES Category (id);
+ALTER TABLE Message_Validation_Rule ADD CONSTRAINT Message_Validation_Rule_Rule
+    FOREIGN KEY (Rule_id)
+    REFERENCES Rule (id);
 ALTER TABLE ReceiverBIC ADD CONSTRAINT ReceiverBIC_BIC_Directory
     FOREIGN KEY (BIC_Directory_BIC)
     REFERENCES BIC_Directory (BIC);
 ALTER TABLE ReceiverBIC ADD CONSTRAINT ReceiverBIC_Transactions
     FOREIGN KEY (SWIFT_Transactions_id)
     REFERENCES Transaction (id);
-ALTER TABLE Message_Validation_Rule ADD CONSTRAINT Rules_Message_Types
+ALTER TABLE RuleLog ADD CONSTRAINT Rule_Log_Message_Log
+    FOREIGN KEY (Message_Validation_Log_id)
+    REFERENCES Message_Validation_Log (id);
+ALTER TABLE RuleLog ADD CONSTRAINT Rule_Log_Message_Rule
+    FOREIGN KEY (Message_Validation_Rule_id)
+    REFERENCES Message_Validation_Rule (id);
+ALTER TABLE Message_Validation_Rule ADD CONSTRAINT Rule_Message_Type
     FOREIGN KEY (Message_Types_id)
     REFERENCES Message_Type (id);
 ALTER TABLE SenderBIC ADD CONSTRAINT SenderBIC_BIC_Directory
     FOREIGN KEY (BIC_Directory_BIC)
     REFERENCES BIC_Directory (BIC);
-ALTER TABLE SenderBIC ADD CONSTRAINT SenderBIC_Transactions
+ALTER TABLE SenderBIC ADD CONSTRAINT SenderBIC_Transaction
     FOREIGN KEY (Transactions_id)
     REFERENCES Transaction (id);
-ALTER TABLE System_Alert ADD CONSTRAINT System_Alerts_System_Logs
-    FOREIGN KEY (SWIFT_System_Logs_id)
-    REFERENCES User_Log (id);
 ALTER TABLE User_Log ADD CONSTRAINT System_Log_Error_M
     FOREIGN KEY (Error_Message_Code)
     REFERENCES Error_Message (Code);
 ALTER TABLE Transaction_Log ADD CONSTRAINT System_Log_Error_Message
     FOREIGN KEY (Error_Message_Code)
     REFERENCES Error_Message (Code);
-ALTER TABLE Transaction_Log ADD CONSTRAINT System_Log_SWIFT_Transaction
+ALTER TABLE TransactionLog ADD CONSTRAINT TransactionLog_Transaction
     FOREIGN KEY (Transaction_id)
     REFERENCES Transaction (id);
-ALTER TABLE User_Log ADD CONSTRAINT System_Log_User
-    FOREIGN KEY (User_id)
-    REFERENCES "User" (id);
-ALTER TABLE User_BIC ADD CONSTRAINT Table_12_BIC_Directory
-    FOREIGN KEY (BIC_Directory_BIC)
-    REFERENCES BIC_Directory (BIC);
-ALTER TABLE User_BIC ADD CONSTRAINT Table_12_User
-    FOREIGN KEY (Users_id)
-    REFERENCES "User" (id);
+ALTER TABLE TransactionLog ADD CONSTRAINT TransactionLog_Transaction_Log
+    FOREIGN KEY (Transaction_Log_id)
+    REFERENCES Transaction_Log (id);
 ALTER TABLE Transaction ADD CONSTRAINT Transaction_Currency
-    FOREIGN KEY (Currency_id)
-    REFERENCES Currency (id);
+    FOREIGN KEY (Currency_CurrencyCode)
+    REFERENCES Currency (CurrencyCode);
 ALTER TABLE Transaction ADD CONSTRAINT Transaction_Message_Type
     FOREIGN KEY (Message_Types_id)
     REFERENCES Message_Type (id);
 ALTER TABLE Transaction ADD CONSTRAINT Transaction_Transaction_Status
-    FOREIGN KEY (Transaction_Status_id)
-    REFERENCES Transaction_Status (id);
-ALTER TABLE User_Access ADD CONSTRAINT User_Access_Control_List
+    FOREIGN KEY (Status_id)
+    REFERENCES Status (id);
+ALTER TABLE UserLog ADD CONSTRAINT UserLog_User
+    FOREIGN KEY (User_id)
+    REFERENCES "User" (id);
+ALTER TABLE UserLog ADD CONSTRAINT UserLog_User_Log
+    FOREIGN KEY (User_Log_id)
+    REFERENCES User_Log (id);
+ALTER TABLE IBAN_BIC ADD CONSTRAINT User_BIC_IBAN
+    FOREIGN KEY (IBAN_id)
+    REFERENCES IBAN (id);
+ALTER TABLE User_Role_Access ADD CONSTRAINT User_Role_Access_Control_List
     FOREIGN KEY (Access_Control_List_id)
     REFERENCES Access_Control_List (id);
-ALTER TABLE User_Access ADD CONSTRAINT User_Access_User
+ALTER TABLE User_Role_Access ADD CONSTRAINT User_Role_Access_IBAN
+    FOREIGN KEY (IBAN_id)
+    REFERENCES IBAN (id);
+ALTER TABLE User_Role_Access ADD CONSTRAINT User_Role_Role
+    FOREIGN KEY (Role_id)
+    REFERENCES Role (id);
+ALTER TABLE User_Role_Access ADD CONSTRAINT User_Role_User
     FOREIGN KEY (User_id)
     REFERENCES "User" (id);
-ALTER TABLE User_Activity ADD CONSTRAINT User_Activity_Activity
-    FOREIGN KEY (Activity_id)
-    REFERENCES Activity (id);
-ALTER TABLE User_Activity ADD CONSTRAINT User_Activity_User
-    FOREIGN KEY (User_id)
-    REFERENCES "User" (id);
-ALTER TABLE User_Log ADD CONSTRAINT User_Log_EventType_User
-    FOREIGN KEY (EventType_User_id)
-    REFERENCES EventType_User (id);
-ALTER TABLE XSD_Schemas ADD CONSTRAINT XSD_Schemas_Message_Type
-    FOREIGN KEY (Message_Type_id)
-    REFERENCES Message_Type (id);
